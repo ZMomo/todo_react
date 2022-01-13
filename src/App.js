4067355baa14aadf9c0d88c687b794d5a38f9f8c
momo <mh.zeghdoudi@gmail.com>
2022-01-13 11:59:22 +0100

    Intégration des composants Focus et Counter dans le routeur (App.js)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Todo from './pages/Todo';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+import Focus from './components/Focus';
+import Counter from './components/Counter';
 
 import './App.css';
 
@@ -16,10 +18,14 @@ const App = () => {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/about">À propos</Link>
+          <Link to="/focus">Focus</Link>
+          <Link to="/counter">counter</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Todo/>} />
           <Route path="/about" element={<About/>} />
+          <Route path="/focus" element={<Focus/>} />
+          <Route path="/counter" element={<Counter initialCount={0} />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
