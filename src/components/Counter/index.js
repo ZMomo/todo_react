845fc74995f0c87c303751f73fb71dd830092184
momo <mh.zeghdoudi@gmail.com>
2022-01-13 15:52:04 +0100

    ajout de deux exemples d'effets avec tableau de dépendance dans le composant Counter

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import useAlert from '../../hooks/useAlert';
 
 
 function Counter({initialCount}) {
     const [count, setCount] = useState(initialCount);
+    const [message, setMessage] = useState();
     
-    useAlert("Mon composant Counter est détruit", count);
+    // useAlert("Mon composant Counter est détruit", count);
+
+    useEffect(() => {
+      alert(`le message a changé: ${message}`)
+    }, [message])
+
+    useEffect(() => {
+      alert("Ce hook ne s'execute qu'une fois au montage de mon composant")
+    }, [])
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setMessage(e.target[0].value)
+    }
 
     return (
       <>
@@ -14,6 +28,11 @@ function Counter({initialCount}) {
         <button onClick={() => setCount(initialCount)}>Réinitialiser</button>
         <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
         <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+        
+        <form onSubmit={handleSubmit}>
+          <input type="text" />
+          <input type="submit" />
+        </form>
       </>
     );
   }
