f04bccf8daba3869cb2d4c71cbb2cabe5fc66cf9
ZMomo <mh.zeghdoudi@gmail.com>
2022-01-27 13:51:25 +0100

    Ajout de la fonctionnalite permettant de poster une todo

--- src/pages/ConnexionBackend/index.js
+++ src/pages/ConnexionBackend/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const ConnexionBackend = () => {
 
     const [todos, setTodos] = useState();
+
+    const textRef = useRef();
 
     const getTodos = async () => {
         const result = await axios.get('http://localhost:8000/api/todos/');
@@ -11,15 +13,35 @@ const ConnexionBackend = () => {
         console.log(result.data.success)
     }
 
+    const postTodo = async (event) => {
+        event.preventDefault(); // empeche la page de se recharger lors du submit du formulaire
+        const result = await axios.post('http://localhost:8000/api/todos/', { text: textRef.current.value })
+        const { insertId } = result.data.success;
+        setTodos(prevState => { 
+            return [ ...prevState, { id: insertId, text: textRef.current.value }]
+        })
+        textRef.current.value = "";
+        textRef.current.focus()
+    }
+
     useEffect(() => {
         getTodos();
+        console.log(textRef.current);
     }, [])
 
+    useEffect(() => {
+        console.log(todos);
+    }, [todos])
+    
     //Cette methode permet de se passer du ' todos && ' dans le second return
     //if (!todos) return <div>loading...</div> 
-
+    
     return (
         <div>
+            <form>
+                <input type="text" ref={textRef}/>
+                <input type="submit" value="creer" onClick={(event) => postTodo(event)}/>
+            </form>
             <ul>
                 {
                     todos && todos.map( 
